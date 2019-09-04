56a4591bc84b36738e701d27496df510f663a463
Darin Webster <darinleenavy@gmail.com>
2019-09-04 09:51:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,27 +10,3 @@ class Shoe
   end
 
 end
-
-
-
-# class Shoe
-#   attr_accessor :color, :size, :material, :condition
-#   attr_reader :brands
-  
-#   BRANDS = []
-
-#   def initialize(brands)
-#     @brands = brands
-#   end
-
-#   def cobble
-#     self.condition = "new"
-#     puts "Your shoe is as good as new!"
-#   end
-
-#   def brands=(brands)
-#     @brands = brands
-#     BRANDS << brands
-#   end
-  
-# end
