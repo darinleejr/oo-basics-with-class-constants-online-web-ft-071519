5be96cccbc1508493f068ff9bcdec5a6e0a682fb
Darin Webster <darinleenavy@gmail.com>
2019-09-04 09:49:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,51 +1,36 @@
-/////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brands
-  
+  attr_reader :brand
+
   BRANDS = []
 
-  def initialize(brands)
-    @brands = brands
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.any? { |b| b == brand }
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
 
-  def brands=(brands)
-    @brands = brands
-    BRANDS << brands
-  end
+
+
+
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brands
   
-end
+#   BRANDS = []
+
+#   def initialize(brands)
+#     @brands = brands
+#   end
+
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+
+#   def brands=(brands)
+#     @brands = brands
+#     BRANDS << brands
+#   end
+  
+# end
